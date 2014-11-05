cb6b1b57bc4d813a79876609a38b7a6fbf0e4c81
gogoyqj <ivugogo@gmail.com>
2014-11-05 12:21:08 +0800

    Update shake.js

--- shake.js
+++ shake.js
@@ -5,7 +5,11 @@
 		document.getElementById('log').insertBefore(p,document.getElementById('log').firstChild)
 	}
 	if('ondevicemotion' in window) {
-		var lastXYZ = {}, // 上一次devicemotion的3个方向的加速度
+		var lastXYZ = {
+				x: 0,
+				y: 0,
+				z: 0
+			}, // 上一次devicemotion的3个方向的加速度
 			threshold = 15, // 临界值，变化量绝对值需要大于临界值 
 			lastTime = Date.now(), // 上一次devicemotion时间
 			dictionary = {
@@ -38,15 +42,7 @@
 	            		pastTime, // 距离上次devicemotion间隔时间
 	            		axle, // 最大增量轴
 	            		direction // 方向
-	            	nowXYZ.z = nowXYZ.z - 9.8 // 忽略重力加速度的Z
-	            	if(lastXYZ.x === null && lastXYZ.y === null && lastXYZ.z === null) {
-	            		lastXYZ = {
-	            			x: nowXYZ.x,
-	            			y: nowXYZ.y,
-	            			z: nowXYZ.z
-	            		}
-	            		return
-	            	}
+	            	nowXYZ.z = nowXYZ.z - 9.8 // 减去重力加速度
 	            	axle = outLimit(nowXYZ.x - lastXYZ.x, nowXYZ.y - lastXYZ.y, nowXYZ.z - lastXYZ.z)
 	            	if(axle) {
 	            		now = Date.now()
