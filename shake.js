a1cf58e80df10616093b7d6aa64aa43961e8d863
gogoyqj <ivugogo@gmail.com>
2014-11-05 12:25:01 +0800

    Update shake.js

--- shake.js
+++ shake.js
@@ -5,11 +5,7 @@
 		document.getElementById('log').insertBefore(p,document.getElementById('log').firstChild)
 	}
 	if('ondevicemotion' in window) {
-		var lastXYZ = {
-				x: 0,
-				y: 0,
-				z: 0
-			}, // 上一次devicemotion的3个方向的加速度
+		var lastXYZ = {}, // 上一次devicemotion的3个方向的加速度
 			threshold = 15, // 临界值，变化量绝对值需要大于临界值 
 			lastTime = Date.now(), // 上一次devicemotion时间
 			dictionary = {
@@ -42,7 +38,15 @@
 	            		pastTime, // 距离上次devicemotion间隔时间
 	            		axle, // 最大增量轴
 	            		direction // 方向
-	            	nowXYZ.z = nowXYZ.z - 9.8 // 减去重力加速度
+	            	nowXYZ.z = nowXYZ.z - 9.8 // 忽略重力加速度的Z
+	            	if(lastXYZ.x === null && lastXYZ.y === null && lastXYZ.z === null) {
+	            		lastXYZ = {
+	            			x: nowXYZ.x,
+	            			y: nowXYZ.y,
+	            			z: nowXYZ.z
+	            		}
+	            		return
+	            	}
 	            	axle = outLimit(nowXYZ.x - lastXYZ.x, nowXYZ.y - lastXYZ.y, nowXYZ.z - lastXYZ.z)
 	            	if(axle) {
 	            		now = Date.now()
@@ -51,7 +55,7 @@
 	            		if(pastTime > 1000) {
 	            			// 单向
 	            			if(lastXYZ[axle] * nowXYZ[axle] <= 0) {
-	            				direction = (nowXYZ[axle] >= 0 ? '-' : '+') + axle
+	            				direction = (lastXYZ[axle] < 0 ? '-' : '+') + axle
 	            			// 双向
 	            			} else {
 	            				direction = axle
